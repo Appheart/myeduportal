3944d603c10ca441c6814e9956a6c488ec80fd6f
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-12-04 13:52:43 +0100

    Updaed menu-navigation color

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,15 +1,20 @@
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faBook, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Menu = () => {
   return (
     <nav className="container mx-auto menu-navigation">
       <ul className="nav-list">
-        <li className="nav-item">
-          <FontAwesomeIcon icon={faHome} /> Home
+        <li className="nav-item active">
+          <FontAwesomeIcon icon={faHome} /> <span>Home</span>
         </li>
-        <li className="nav-item">Credentials</li>
-        <li className="nav-item">Profile</li>
+        <li className="nav-item">
+          <FontAwesomeIcon icon={faBook} /> <span> Credentials</span>
+        </li>
+        <li className="nav-item">
+          <FontAwesomeIcon icon={faUser} /> <span>Profile</span>{' '}
+        </li>
       </ul>
     </nav>
   );
