cf250ee6f0994015e70d564b6d62042845c9ac46
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-12-04 12:00:02 +0100

    ...

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,8 +1,13 @@
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Menu = () => {
   return (
     <nav className="container mx-auto menu-navigation">
       <ul className="nav-list">
-        <li className="nav-item">Home</li>
+        <li className="nav-item">
+          <FontAwesomeIcon icon={faHome} /> Home
+        </li>
         <li className="nav-item">Credentials</li>
         <li className="nav-item">Profile</li>
       </ul>
