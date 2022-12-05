e3ebe99610714a64278c088cbb3ba435ae319f1c
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-12-05 06:36:17 +0100

    UPDATED GATEWAY

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Header, Menu } from '../components';
 
 const Layout = () => {
+  const { userName } = useSelector((state) => state.auth);
+  const [token, setToken] = useState(userName);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+  }, []);
+
+  useEffect(() => {
+    setToken(() => userName);
+  }, [userName]);
+
+  console.log(userName);
+
   return (
     <>
       <Header />
