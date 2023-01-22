56bc8fe9f0cca5da285301a440480ddb512f83c2
mrpaulishaili <paulishail@gmail.com>
2023-01-22 13:06:54 +0100

    ,..

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -33,11 +33,12 @@ const Profile = () => {
         <UserError message={'Profile not setup'} cta={'Setup profile'} />
       ) : (
         <>
-          <div className="drawn-bg bg-[url('https://img.freepik.com/premium-vector/painted-background-multicoloured-palette_23-2148427592.jpg?w=800')] h-[30vh] w-full bg-center bg-cover "></div>
-
-          <fieldset className="-mt-[10vh] flex flex-col gap-5 py-5">
-            <div className="flex items-end gap-5">
-              <div className="profile__image--container  h-[8rem] w-[8rem] shadow-md rounded-full overflow-hidden">
+          <div className=" h-[23vh] md:h-[30vh] w-full">
+            <div className="drawn-bg bg-[url('https://img.freepik.com/premium-vector/painted-background-multicoloured-palette_23-2148427592.jpg?w=800')] h-full w-full bg-center bg-cover "></div>
+          </div>
+          <fieldset className="-mt-[9vh] flex flex-col gap-5 py-5 border-b-2 border-red-300">
+            <div className="flex items-center gap-3">
+              <div className="profile__image--container h-[8rem] w-[8rem] shadow-md rounded-full overflow-hidden">
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfsTQFb0kp8I5e3JYPbVszsdPRsHBp3MM0snd7GltdQQ&s"
                   alt="Profile image"
@@ -46,73 +47,109 @@ const Profile = () => {
                   className={'w-full h-full object-cover'}
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col mt-9 leading-tight">
                 <h3 className="font-bold text-xl">{user?.name}</h3>
-                <p className="text-sm">2021/234544</p>
-                <p className="text-sm">Robotics Engineering</p>
+                <p className="text-xs">Robotics Engineering</p>
+                <p className="text-xs">@username</p>
               </div>
             </div>
           </fieldset>
 
-          <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
-            <fieldset className="fieldset flex flex-col justify-between gap-5 ">
-              <table class="table-auto gap-2 min-h-[10rem] justify-self-start">
-                <thead className="text-left text-sm mb-2">
-                  <th>Course</th>
-                  <th>Title</th>
-                </thead>
-                <tbody className="overflow-y-scroll ">
-                  {courses.map((course) => (
-                    <tr className="text-sm mb-2">
-                      <td>{course.code}</td>
-                      <td>{course.name}</td>
-                      <td>
-                        <LinkBtn link={`/courses/${course.short}`}>
-                          View
-                        </LinkBtn>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+          <section className="bg-gradient-to-r from-sky-100 to-indigo-200 container mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 p-3 rounded-lg">
+            {/*  */}
 
-              <div className="flex gap-2 justify-between">
+            <fieldset className="fieldset flex flex-col justify-between gap-5 relative bg-white ">
+              <div className="school_logo absolute top-3 right-3 shadow-sm rounded-lg">
                 <LinkBtn
                   link={'/'}
-                  className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
+                  className="bg-green-300 text-gray-900 max-w-max ml-auto flex px-2 py-2 text-sm text-center rounded-md gap-3 items-center justify-center"
                 >
                   <FontAwesomeIcon icon={faPenAlt} />
                   Edit
-                </LinkBtn>
-                <LinkBtn
-                  link={'/'}
-                  className="w-full bg-green-500 text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
-                >
-                  <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
                 </LinkBtn>
               </div>
+              <h3 className="font-bold text-green-800">Bio Data:</h3>
+              <div className="grid grid-cols-2 gap-2 text-sm">
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> Matric:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> JAMB:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> O'Level:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> NIN:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+              </div>
             </fieldset>
-            <h3>My Credentials</h3>
 
-            <UserError
-              message={"You haven't uploaded any files"}
-              cta={'Get started now'}
-            />
-            <fieldset className="fieldset flex flex-col justify-between gap-5 ">
-              <div className="flex gap-2 justify-between">
+            {/*  */}
+            <fieldset className="fieldset flex flex-col justify-between gap-5 relative  bg-white">
+              <div className="school_logo absolute top-3 right-3 shadow-sm rounded-lg">
                 <LinkBtn
                   link={'/'}
-                  className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
+                  className="bg-green-300 text-gray-900 max-w-max ml-auto flex px-2 py-2 text-sm text-center rounded-md gap-3 items-center justify-center"
                 >
                   <FontAwesomeIcon icon={faPenAlt} />
                   Edit
                 </LinkBtn>
-                <LinkBtn
-                  link={'/'}
-                  className="w-full bg-green-500  text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
-                >
-                  <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
-                </LinkBtn>
+              </div>
+              <h3 className="font-bold text-green-8">Contact:</h3>
+              <div className="grid grid-cols-2 gap-2 text-sm ">
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> Phone:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> Whatsapp:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> Email:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between ">
+                  <div className="">
+                    <p className="font-bold"> Facebook:</p>
+                  </div>
+                  <div className="w-full flex justify-start">
+                    <p className="font-mono">Nil</p>
+                  </div>
+                </div>
               </div>
             </fieldset>
           </section>
