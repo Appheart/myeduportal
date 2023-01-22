a36785cf42e710b7731e69a23849406a96cd2560
mrpaulishaili <paulishail@gmail.com>
2023-01-22 11:00:30 +0100

    Update profile layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,9 +32,11 @@ const Profile = () => {
       {!userName ? (
         <UserError message={'Profile not setup'} cta={'Setup profile'} />
       ) : (
-        <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
-          <fieldset className="fieldset flex  flex-col gap-5 py-5">
-            <div className="flex items-center gap-5">
+        <>
+          <div className="drawn-bg bg-[url('https://img.freepik.com/premium-vector/painted-background-multicoloured-palette_23-2148427592.jpg?w=800')] h-[30vh] w-full bg-center bg-cover "></div>
+
+          <fieldset className="-mt-[10vh] flex flex-col gap-5 py-5">
+            <div className="flex items-end gap-5">
               <div className="profile__image--container  h-[8rem] w-[8rem] shadow-md rounded-full overflow-hidden">
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfsTQFb0kp8I5e3JYPbVszsdPRsHBp3MM0snd7GltdQQ&s"
@@ -46,89 +48,75 @@ const Profile = () => {
               </div>
               <div className="flex flex-col">
                 <h3 className="font-bold text-xl">{user?.name}</h3>
-                <p className="text-sm">100 Level</p>
+                <p className="text-sm">2021/234544</p>
                 <p className="text-sm">Robotics Engineering</p>
-                <p className="text-sm">University of Nigeria, Nsukka</p>
               </div>
             </div>
-
-            <div className="flex gap-2 justify-between">
-              <LinkBtn
-                link={'/'}
-                className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
-              >
-                <FontAwesomeIcon icon={faPenAlt} />
-                Edit
-              </LinkBtn>
-              <LinkBtn
-                link={'/'}
-                className="w-full bg-green-500 text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
-              >
-                <FontAwesomeIcon icon={faMoon} />
-                Change Theme
-              </LinkBtn>
-            </div>
           </fieldset>
 
-          <fieldset className="fieldset flex flex-col justify-between gap-5 ">
-            <table class="table-auto gap-2 min-h-[10rem] justify-self-start">
-              <thead className="text-left text-sm mb-2">
-                <th>Course</th>
-                <th>Title</th>
-              </thead>
-              <tbody className="overflow-y-scroll ">
-                {courses.map((course) => (
-                  <tr className="text-sm mb-2">
-                    <td>{course.code}</td>
-                    <td>{course.name}</td>
-                    <td>
-                      <LinkBtn link={`/courses/${course.short}`}>View</LinkBtn>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
+            <fieldset className="fieldset flex flex-col justify-between gap-5 ">
+              <table class="table-auto gap-2 min-h-[10rem] justify-self-start">
+                <thead className="text-left text-sm mb-2">
+                  <th>Course</th>
+                  <th>Title</th>
+                </thead>
+                <tbody className="overflow-y-scroll ">
+                  {courses.map((course) => (
+                    <tr className="text-sm mb-2">
+                      <td>{course.code}</td>
+                      <td>{course.name}</td>
+                      <td>
+                        <LinkBtn link={`/courses/${course.short}`}>
+                          View
+                        </LinkBtn>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
-            <div className="flex gap-2 justify-between">
-              <LinkBtn
-                link={'/'}
-                className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
-              >
-                <FontAwesomeIcon icon={faPenAlt} />
-                Edit
-              </LinkBtn>
-              <LinkBtn
-                link={'/'}
-                className="w-full bg-green-500 text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
-              >
-                <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
-              </LinkBtn>
-            </div>
-          </fieldset>
-          <h3>My Credentials</h3>
+              <div className="flex gap-2 justify-between">
+                <LinkBtn
+                  link={'/'}
+                  className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
+                >
+                  <FontAwesomeIcon icon={faPenAlt} />
+                  Edit
+                </LinkBtn>
+                <LinkBtn
+                  link={'/'}
+                  className="w-full bg-green-500 text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
+                >
+                  <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
+                </LinkBtn>
+              </div>
+            </fieldset>
+            <h3>My Credentials</h3>
 
-          <UserError
-            message={"You haven't uploaded any files"}
-            cta={'Get started now'}
-          />
-          <fieldset className="fieldset flex flex-col justify-between gap-5 ">
-            <div className="flex gap-2 justify-between">
-              <LinkBtn
-                link={'/'}
-                className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
-              >
-                <FontAwesomeIcon icon={faPenAlt} />
-                Edit
-              </LinkBtn>
-              <LinkBtn
-                link={'/'}
-                className="w-full bg-green-500 text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
-              >
-                <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
-              </LinkBtn>
-            </div>
-          </fieldset>
-        </section>
+            <UserError
+              message={"You haven't uploaded any files"}
+              cta={'Get started now'}
+            />
+            <fieldset className="fieldset flex flex-col justify-between gap-5 ">
+              <div className="flex gap-2 justify-between">
+                <LinkBtn
+                  link={'/'}
+                  className="bg-green-300 text-gray-900 w-full flex px-2 py-1 text-sm text-center rounded-md gap-5 items-center justify-center"
+                >
+                  <FontAwesomeIcon icon={faPenAlt} />
+                  Edit
+                </LinkBtn>
+                <LinkBtn
+                  link={'/'}
+                  className="w-full bg-green-500  text-white flex  px-2 py-1 text-sm text-center rounded-md items-center gap-5 justify-center"
+                >
+                  <FontAwesomeIcon icon={faPrint} /> <span>Print</span>
+                </LinkBtn>
+              </div>
+            </fieldset>
+          </section>
+        </>
       )}
     </main>
   );
