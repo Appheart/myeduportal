6bee458f8ee07a94c5e0643fc28c5d69ab433f15
mrpaulishaili <paulishail@gmail.com>
2023-01-22 01:05:52 +0100

    ...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,35 +4,33 @@ import Logo from '../components/Logo';
 
 const Login = () => {
   return (
-    <div
+    <main
       id="LoginPage"
       className="flex flex-col items-center justify-center p-4 space-y-4 antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light"
     >
-      <main>
-        <div className="w-full max-w-sm px-4 py-6 space-y-5 bg-white rounded-md dark:bg-darker">
-          <Logo />
+      <div className="w-full max-w-sm px-4 py-[5%] space-y-5 bg-white rounded-md dark:bg-darker">
+        <Logo />
 
-          <LoginForm />
+        <LoginForm />
 
-          {/* <!-- Or --> */}
-          <div className="flex items-center justify-center space-x-2 flex-nowrap">
-            <span className="w-20 h-px bg-gray-300"></span>
-            <span>OR</span>
-            <span className="w-20 h-px bg-gray-300"></span>
-          </div>
-
-          {/* <!-- Social login links --> */}
-
-          {/* <!-- Register link --> */}
-          <LinkBtn
-            link={'/'}
-            className="text-sm text-center flex underline justify-center text-gray-600 dark:text-gray-400"
-          >
-            Get onboard
-          </LinkBtn>
+        {/* <!-- Or --> */}
+        <div className="flex items-center justify-center space-x-2 flex-nowrap">
+          <span className="w-20 h-px bg-gray-300"></span>
+          <span>OR</span>
+          <span className="w-20 h-px bg-gray-300"></span>
         </div>
-      </main>
-    </div>
+
+        {/* <!-- Social login links --> */}
+
+        {/* <!-- Register link --> */}
+        <LinkBtn
+          link={'/'}
+          className="text-sm text-center flex underline justify-center text-gray-600 dark:text-gray-400"
+        >
+          Get onboard
+        </LinkBtn>
+      </div>
+    </main>
   );
 };
 
