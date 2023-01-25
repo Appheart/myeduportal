e2fc550f6058e2b002bb15c9e79552956388896c
mrpaulishaili <paulishail@gmail.com>
2023-01-25 07:16:56 +0100

    Added today indicator on calendar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,18 +1,49 @@
 import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
-import { faGraduationCap, faPenAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGraduationCap,
+  faPenAlt,
+  faTableCells,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from '@mui/material';
 import { useAuth } from '../../app/hooks/useAuth';
 import { LinkBtn } from '../../components';
 
 const Profile = () => {
   const { user, logout } = useAuth();
 
+  const weekDays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  const d = new Date();
+
+  const dd = new Date().toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
+  let today = weekDays[d.getDay() - 1];
+
+  const active = (day) => {
+    if (day === today)
+      return 'bg-yellow-200 hover:bg-yellow-600 text-gray-600 hover:text-gray-100 ';
+    else return 'bg-white hover:bg-slate-600 text-gray-600 hover:text-gray-100';
+  };
+
   return (
     <main>
       <div className=" h-[23vh] md:h-[30vh] w-full">
         <div className="drawn-bg bg-[url('https://img.freepik.com/premium-vector/painted-background-multicoloured-palette_23-2148427592.jpg?w=800')] h-full w-full bg-center bg-cover "></div>
       </div>
+
       <fieldset className="-mt-[9vh] flex flex-col gap-5 py-5 border-b-2 border-red-300">
         <div className="flex items-center gap-3">
           <div className="profile__image--container h-[8rem] w-[8rem] shadow-md rounded-full overflow-hidden">
@@ -35,7 +66,7 @@ const Profile = () => {
 
       <section className="">
         {/*  */}
-        <div className="ontainer mx-auto rounded-lg grid grid-cols-1 md:grid-cols-2 gap-3 p-3">
+        <div className="ontainer mx-auto rounded-lg grid grid-cols-1 md:grid-cols-2 gap-3">
           <fieldset className="fieldset flex flex-col justify-between gap-3 relative bg-white ">
             <div className="school_logo absolute top-3 right-3 shadow-sm rounded-lg">
               <LinkBtn
@@ -101,38 +132,29 @@ const Profile = () => {
               <p>My Courses</p>
             </h3>
             <div className="flex flex-wrap gap-2 text-xs  text-gray-600">
-              <LinkBtn
-                link={`/courses/BIO101`}
-                className={'p-2 bg-yellow-50 rounded-md font-bold'}
-              >
-                Bio 101
-              </LinkBtn>
-              <LinkBtn
-                link={'/profile'}
-                className={'p-2 bg-yellow-50 rounded-md font-bold'}
-              >
-                COS 101
-              </LinkBtn>
-              <LinkBtn
-                link={'/profile'}
-                className={'p-2 bg-yellow-50 rounded-md font-bold'}
-              >
-                COS 101
-              </LinkBtn>
-              <LinkBtn
-                link={'/profile'}
-                className={'p-2 bg-yellow-50 rounded-md font-bold'}
-              >
-                COS 101
-              </LinkBtn>
+              {[
+                'COS 101',
+                'GSP 111',
+                'GSP 101',
+                'BIO 151',
+                'CHM 101',
+                'CHM 171',
+                'PHY 191',
+              ].map((c, index) => (
+                <LinkBtn
+                  key={index}
+                  link={`/courses/BIO101`}
+                  className={'p-2 bg-yellow-50 rounded-md font-bold'}
+                >
+                  {c}
+                </LinkBtn>
+              ))}
             </div>
           </fieldset>
-        </div>
 
-        {/*  */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
           {/* Time table */}
-          <fieldset className="fieldset flex flex-col gap-3 relative bg-white ">
+
+          <fieldset className="fieldset flex flex-col gap-3 relative bg-yellow-50 ">
             <div className="school_logo absolute top-3 right-3 shadow-sm rounded-lg">
               <LinkBtn
                 link={'/'}
@@ -148,20 +170,25 @@ const Profile = () => {
               <p>My Timetable</p>
             </h3>
             <div className="w-full mx-auto space-y-5">
-              {[
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-                'Sunday',
-              ].map((day) => (
-                <details className="rounded-lg shadow-sm py-2">
-                  <summary className="text-md py-2 text-gray-50 bg-yellow-700 px-3 font-bold rounded-tl-md rounded-tr-md">
-                    {day}
+              {weekDays.map((day, index) => (
+                <details className="rounded-lg shadow-sm" key={index}>
+                  <summary
+                    className={`text-md py-2   px-3 font-bold rounded-tl-md rounded-tr-md cursor-pointer flex justify-between items-center ${active(
+                      day
+                    )}`}
+                  >
+                    {day == today ? (
+                      <>
+                        {dd}
+                        <span className="text-xs bg-orange-500 py-0.5 px-1 rounded-md text-white">
+                          <FontAwesomeIcon icon={faTableCells} />
+                        </span>
+                      </>
+                    ) : (
+                      day
+                    )}
                   </summary>
-                  <div className="mt-3 px-3">
+                  <div className=" px-3 py-2 pt-5 bg-white">
                     <p className="text-sm leading-normal">None yet</p>
                   </div>
                 </details>
@@ -169,6 +196,7 @@ const Profile = () => {
             </div>
           </fieldset>
         </div>
+
         <button
           className="max-w- mt-5 mb-5 px-4 py-2 flex bg-red-700 text-white rounded-md mx-auto"
           onClick={logout}
