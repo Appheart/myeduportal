92c7eb6ab4e28cf1b1cd780df676c17acdd30714
mrpaulishaili <paulishail@gmail.com>
2023-01-25 03:02:55 +0100

    ...

--- src/app/api/newsApi.js
+++ src/app/api/newsApi.js
@@ -1,7 +1,7 @@
 import { api } from '.';
 
 export const getAllNews = async (pageParam = 1) => {
-  const response = await api.get(`/news?pageNumber=${pageParam}`);
+  const response = await api.get(`/news?page=${pageParam}`);
 
   return response.data;
 };
