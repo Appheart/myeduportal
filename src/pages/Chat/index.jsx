b3724dda38abb1fe6a5621ee682bdcd45d5413ac
mrpaulishaili <paulishail@gmail.com>
2023-01-31 03:31:38 +0100

    ...

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Chats from '../../screens/Chat/Chats';
 import ChatView from '../../screens/Chat/ChatView';
 
-import bg from '../../assets/images/bg-image.png';
 import { Menu } from '../../components';
 
 const ChatPage = () => {
@@ -16,9 +15,8 @@ const ChatPage = () => {
     <>
       <main className="bg-[url(/images/bg-image.png)] min-h-screen grid place-items-center bg-fixed h-full">
         {chatId && (
-          <section className="w-full md:w-[97.5vw] h-[85vh] bg-red-50 flex flex-row">
-            <Chats />
-            <ChatView isMobile={isMobile} />
+          <section className="grid place-items-center">
+            <h3>Feature on premium</h3>
           </section>
         )}
       </main>{' '}
