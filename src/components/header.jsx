754b48729d4b5151292873468118a1f78f5d64ec
mrpaulishaili <paulishail@gmail.com>
2023-02-10 11:19:12 +0100

    ...

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,8 @@
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBell,
-  faEnvelopeOpenText,
-  faSearch,
-  faShop,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import NotificationDrawer from './notificationDrawer';
+import { faBell, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import Logo from './Logo';
 import LinkBtn from './Link';
-import { icon } from '@fortawesome/fontawesome-svg-core';
 
 const Header = () => {
   const [notifyOpen, setNotifyOpen] = useState(false);
@@ -31,37 +22,57 @@ const Header = () => {
           <Logo />
         </div>
 
-        <div className="flex gap-4 items-center">
-          <div className="icon search-icon border border-green-200 p-1 rounded-full h-9 w-9 flex items-center justify-center hover:bg-green-600 cursor-pointer mr-4 hover:text-white">
-            <FontAwesomeIcon
-              icon={faSearch}
-              size="lg"
-              className="text-green-600"
-            />
-          </div>
+        <div className="flex gap-5 items-center">
+          <nav className="hidden sm:flex gap-3 font-bold text-gray-700">
+            <LinkBtn link="/" className={'font-bold text-gray-700'}>
+              Quizzes
+            </LinkBtn>
+            <LinkBtn link="/" className={'font-bold text-gray-700'}>
+              Videos
+            </LinkBtn>
+            <LinkBtn link="/" className={'font-bold text-gray-700'}>
+              News
+            </LinkBtn>
+            <LinkBtn link="/" className={'font-bold text-gray-700'}>
+              Shopping
+            </LinkBtn>
+            <LinkBtn link="/" className={'font-bold text-gray-700'}>
+              Fudera
+            </LinkBtn>
+          </nav>
 
-          <div
-            className={
-              notifyOpen
-                ? 'notification open rounded-full w-3 h-3'
-                : 'notification rounded-full w-3 h-3'
-            }
-            onClick={toggleNotification}
-          >
-            <FontAwesomeIcon icon={faBell} className="" />
-            <span>3</span>
-          </div>
+          <div className="flex gap-3 items-center">
+            <div className="icon search-icon border border-green-200 p-1 rounded-full h-9 w-9 flex items-center justify-center hover:bg-green-600 cursor-pointer mr-4 hover:text-white">
+              <FontAwesomeIcon
+                icon={faSearch}
+                size="lg"
+                className="text-green-600"
+              />
+            </div>
 
-          <LinkBtn
-            link={'/profile'}
-            className="notification rounded-full overflow-hidden"
-          >
-            <img
-              loading="lazy"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfsTQFb0kp8I5e3JYPbVszsdPRsHBp3MM0snd7GltdQQ&s "
-              alt=""
-            />
-          </LinkBtn>
+            <div
+              className={
+                notifyOpen
+                  ? 'notification open rounded-full w-3 h-3'
+                  : 'notification rounded-full w-3 h-3'
+              }
+              onClick={toggleNotification}
+            >
+              <FontAwesomeIcon icon={faBell} className="" />
+              <span>3</span>
+            </div>
+
+            <LinkBtn
+              link={'/profile'}
+              className="notification rounded-full overflow-hidden"
+            >
+              <img
+                loading="lazy"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfsTQFb0kp8I5e3JYPbVszsdPRsHBp3MM0snd7GltdQQ&s "
+                alt=""
+              />
+            </LinkBtn>
+          </div>
         </div>
       </div>
     </header>
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,23 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
-import {
-  faMessage,
-  faStar,
-  faSun,
-  faUser,
-} from '@fortawesome/free-regular-svg-icons';
-import {
-  faBook,
-  faBraille,
-  faHandDots,
-  faHome,
-  faWallet,
-} from '@fortawesome/free-solid-svg-icons';
+import { faMessage, faSun } from '@fortawesome/free-regular-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Menu = () => {
   return (
-    <nav className="container mx-auto menu-navigation border-t-4">
+    <nav className="md:hidden container mx-auto menu-navigation border-t-4">
       <ul className="nav-list">
         <li className="nav-item">
           <NavLink to={'/'}>
@@ -26,17 +15,12 @@ const Menu = () => {
         </li>
         <li className="nav-item ">
           <NavLink to={'/chat'}>
-            <FontAwesomeIcon icon={faMessage} /> <span> Chat</span>
+            <FontAwesomeIcon icon={faMessage} /> <span> AMA</span>
           </NavLink>
         </li>
         <li className="nav-item">
           <NavLink to={'/explore'}>
             <FontAwesomeIcon icon={faSun} /> <span>Explore</span>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={'/walleti'}>
-            <FontAwesomeIcon icon={faBraille} /> <span>More</span>
           </NavLink>
         </li>
       </ul>
